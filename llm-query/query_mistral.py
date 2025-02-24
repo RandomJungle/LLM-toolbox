b0d9058eb04939d71473d0c6a1307268425c510a
Juliette Bourquin <bourquinjuliette@gmail.com>
2025-02-24 14:43:01 +0100

    first commit with templates, mistral query and gitignore

---
+++ llm-query/query_mistral.py
@@ -0,0 +1,229 @@
+import os
+from typing import Optional, List, Dict
+
+import pandas as pd
+from dotenv import find_dotenv, load_dotenv
+from mistralai import Mistral
+from tqdm import tqdm
+
+from converter import convert_content_to_json
+from preprocessing import (
+    chunk_dataframe,
+    read_prompt_file,
+)
+
+
+def query_embeddings(
+        dataframe: pd.DataFrame,
+        model_name: Optional[str] = 'mistral-embed',
+        num_chunks: Optional[int] = 1) -> pd.DataFrame:
+
+    data = dataframe[['id', 'text']]
+    chunks = chunk_dataframe(data, num_chunks)
+    outputs = []
+
+    client = Mistral(
+        api_key=os.getenv('MISTRAL_API_KEY', '')
+    )
+    for chunk in tqdm(chunks):
+        chunk_response = client.embeddings.create(
+            model=model_name,
+            inputs=chunk['text'].tolist()
+        )
+        chunk[f'{model_name}_embeddings'] = [emb.embedding for emb in chunk_response.data]
+        outputs.append(chunk)
+
+    output_dataframe = pd.concat(outputs, ignore_index=True)
+    merged = pd.merge(
+        left=dataframe,
+        right=output_dataframe[['id', f'{model_name}_embeddings']],
+        on='id',
+        how='left',
+        validate='1:1'
+    )
+    return merged
+
+
+def query_with_dataframe(
+        dataframe: pd.DataFrame,
+        prompt: Dict,
+        model_name: Optional[str] = 'mistral-large-latest',
+        temperature: Optional[float] = 0.2,
+        num_chunks: Optional[int] = 1,
+        stream: Optional[bool] = False) -> pd.DataFrame:
+
+    data = dataframe[['id', 'text']]
+    chunks = chunk_dataframe(data, num_chunks)
+    responses = []
+
+    client = Mistral(
+        api_key=os.getenv('MISTRAL_API_KEY', '')
+    )
+
+    for chunk in tqdm(chunks):
+        chunk_json = chunk.to_json(orient='records')
+        messages = [
+            {
+                'content': prompt.get('role'),
+                'role': 'system',
+            },
+            {
+                'content': prompt.get('content') + f'<t>{chunk_json}</t>',
+                'role': 'user',
+            },
+        ]
+        if not stream:
+            response = client.chat.complete(
+                model=model_name,
+                messages=messages,
+                temperature=temperature,
+                response_format={
+                    'type': 'json_object',
+                }
+            )
+            content = response.choices[0].message.content
+        else:
+            response = client.chat.stream(
+                model=model_name,
+                messages=messages,
+                temperature=temperature,
+                response_format={
+                    'type': 'json_object',
+                }
+            )
+            collected_messages = []
+            for res_chunk in response:
+                chunk_content = res_chunk.data.choices[0].delta.content
+                collected_messages.append(chunk_content)
+                print(chunk_content)
+            content = ''.join([m for m in collected_messages if m is not None])
+        mini_df = pd.DataFrame.from_records(
+            convert_content_to_json(content)
+        )
+        responses.append(mini_df)
+
+    output_dataframe = pd.concat(responses, ignore_index=True)
+    merged = pd.merge(
+        left=dataframe,
+        right=output_dataframe[prompt.get('output_columns')],
+        on='id',
+        how='left'
+    )
+    return merged
+
+
+def query_with_documents(
+        documents: Dict[str, str],
+        prompt: Dict,
+        output_path: str,
+        model_name: Optional[str] = 'mistral-large-latest',
+        temperature: Optional[float] = 0.2,
+        stream: Optional[bool] = False) -> None:
+
+    client = Mistral(
+        api_key=os.getenv('MISTRAL_API_KEY', '')
+    )
+
+    for document_name, document in tqdm(documents.items()):
+
+        messages = [
+            {
+                'content': prompt.get('role'),
+                'role': 'system',
+            },
+            {
+                'content': prompt.get('content') + f'<doc>{document}</doc>',
+                'role': 'user',
+            },
+        ]
+        if not stream:
+            response = client.chat.complete(
+                model=model_name,
+                messages=messages,
+                temperature=temperature
+            )
+            content = response.choices[0].message.content
+        else:
+            response = client.chat.stream(
+                model=model_name,
+                messages=messages,
+                temperature=temperature
+            )
+            collected_messages = []
+            for res_chunk in response:
+                chunk_content = res_chunk.data.choices[0].delta.content
+                collected_messages.append(chunk_content)
+                print(chunk_content)
+            content = ''.join([m for m in collected_messages if m is not None])
+
+        with open(output_path, 'a') as output_file:
+            output_file.write(
+                f'## {document_name}\n\n{content}\n\n\n\n\n'
+            )
+
+
+def query_with_document(
+        document: str,
+        prompt: Dict,
+        output_path: str,
+        stream: Optional[bool] = False) -> None:
+
+    client = Mistral(
+        api_key=os.getenv('MISTRAL_API_KEY', '')
+    )
+
+    model_name = prompt.get('model')
+    temperature = prompt.get('temperature')
+
+    messages = [
+        {
+            'content': prompt.get('role'),
+            'role': 'system',
+        },
+        {
+            'content': prompt.get('content').replace('[document]', document),
+            'role': 'user',
+        },
+    ]
+    if not stream:
+        response = client.chat.complete(
+            model=model_name,
+            messages=messages,
+            temperature=temperature
+        )
+        content = response.choices[0].message.content
+    else:
+        response = client.chat.stream(
+            model=model_name,
+            messages=messages,
+            temperature=temperature
+        )
+        collected_messages = []
+        for res_chunk in response:
+            chunk_content = res_chunk.data.choices[0].delta.content
+            collected_messages.append(chunk_content)
+            print(chunk_content)
+        content = ''.join([m for m in collected_messages if m is not None])
+
+    with open(output_path, 'w') as output_file:
+        output_file.write(content)
+
+
+if __name__ == '__main__':
+
+    load_dotenv(find_dotenv())
+
+    task = 'redaction'
+    version = '1.5'
+
+    prompt_dict = read_prompt_file(
+        os.getenv('PROMPT_FILE_PATH'),
+        task=task,
+        version=version
+    )
+    query_with_document(
+        document='',
+        prompt=prompt_dict,
+        stream=True,
+        output_path=f'/home/juliette/projects/llm_toolbox/outputs/{version}.txt'
+    )
---
+++ preprocessing.py
@@ -0,0 +1,75 @@
+import json
+import os
+from typing import List, Optional, Dict
+
+import pandas as pd
+import pdfplumber
+
+
+def read_json_dataframe(
+        file_path: str,
+        remove_duplicates: bool,
+        orient: Optional[str] = 'table') -> pd.DataFrame:
+    if file_path.endswith('.jsonl'):
+        dataframe = pd.read_json(
+            path_or_buf=file_path,
+            lines=True
+        )
+    else:
+        dataframe = pd.read_json(
+            path_or_buf=file_path,
+            orient=orient
+        )
+    if remove_duplicates:
+        dataframe = dataframe.drop_duplicates(subset='id')
+    return dataframe
+
+
+def read_prompt_file(
+        file_path: str,
+        task: str,
+        version: Optional[str] = 'latest') -> Dict:
+    with open(file_path, 'r') as prompt_file:
+        prompt_dict = json.loads(prompt_file.read())
+    task_dict = prompt_dict.get(task)
+    if version == 'latest':
+        selected_prompt = task_dict.get(max(task_dict.keys()))
+    else:
+        selected_prompt = task_dict.get(version)
+    return selected_prompt
+
+
+def convert_pdf2txt(pdf_path: str):
+    texts = []
+    with pdfplumber.open(pdf_path) as pdf_file:
+        for page in pdf_file.pages:
+            texts.append(page.extract_text())
+    return ('\n' * 10).join(texts)
+
+
+def read_pdfs(bibliography_path: str):
+    bibliography = dict()
+    for file_name in os.listdir(bibliography_path):
+        bibliography[file_name] = convert_pdf2txt(
+            pdf_path=os.path.join(bibliography_path, file_name)
+        )
+    return bibliography
+
+
+def chunk_dataframe(
+        dataframe: pd.DataFrame,
+        num_chunks: int) -> List[pd.DataFrame]:
+    """
+    Split dataframe into n chunks (mainly for LLM querying)
+    """
+    if num_chunks <= 1:
+        chunks = [dataframe]
+    else:
+        rows_per_chunk = len(dataframe) // num_chunks
+        chunks = [
+            dataframe[i * rows_per_chunk:(i + 1) * rows_per_chunk]
+            for i in range(num_chunks)
+        ]
+        if len(dataframe) % num_chunks != 0:
+            chunks.append(dataframe[num_chunks * rows_per_chunk:])
+    return chunks
